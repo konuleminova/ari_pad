4eb1ced426878ddfbfe72f18318f60a1a94dcd73
konuleminova <konul.eminova39@gmail.com>
2020-11-23 12:00:49 +0400

    login viewmodel done

--- lib/business_logic/routes/route_names.dart
+++ lib/business_logic/routes/route_names.dart
@@ -1,8 +1,5 @@
 import 'package:ari_pad/business_logic/view_models/LoginViewModel.dart';
 import 'package:ari_pad/business_logic/view_models/OrderListViewModel.dart';
-import 'package:ari_pad/ui/views/home/home.dart';
-import 'package:ari_pad/ui/views/orderlist/order_list.dart';
-import 'package:flutter/material.dart';
 
 const ROUTE_LOGIN = '/';
 final routeNames = {
