34f9bc1f8873e87be7b0c7aaecc5cd2e1d462d43
konuleminova <konul.eminova39@gmail.com>
2020-12-03 10:47:17 +0400

    login viewmodel done

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ari_pad/ui/main.dart';
+import 'package:ari_pad/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
